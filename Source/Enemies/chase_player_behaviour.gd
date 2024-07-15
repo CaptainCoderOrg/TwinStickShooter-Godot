47611b38740f432c284a7578e3a4cda61dab71bc
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-15 05:32:33 -0700

    feat: Add chase movement to neanderthal.

--- Source/Enemies/chase_player_behaviour.gd
+++ Source/Enemies/chase_player_behaviour.gd
@@ -1,9 +1,16 @@
 class_name ChasePlayerBehaviour
 extends Node
 
+@export var min_speed : float = 1
+@export var max_speed : float = 3
+@export var speed : float
 @onready var character_body : CharacterBody2D = owner
 @onready var body : Node2D = %Body
+@onready var move_target : Node2D = %MoveTarget
 var target : Node2D
+
+func _ready() -> void:
+	speed = randf_range(min_speed, max_speed)
 
 func acquire_target() -> void:
 	if not target:
@@ -14,3 +21,5 @@ func acquire_target() -> void:
 func _physics_process(_delta : float) -> void:
 	acquire_target()
 	body.look_at(target.global_position)
+	character_body.velocity = (move_target.global_position - character_body.global_position).normalized() * speed * ArenaManager.SPEED
+	character_body.move_and_slide()	
